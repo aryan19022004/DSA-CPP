5fc767a0da316298b104737fbe595d9198c7f984
Aryan Tiwari <146500011+aryan19022004@users.noreply.github.com>
2026-03-03 13:53:05 +0530

    Add C++ program for longest subarray with sum k
    Implement a program to find the longest subarray with a given sum.

---
+++ longest-sub-array-with-sum-k.cpp
@@ -0,0 +1,29 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<vector>
+using namespace std;
+
+
+int main() {
+    vector<int> v = {2,9,8,7,5,8,5,5,5,5,5,4};
+    vector<int> result;
+    int target  = 29;
+    int n = v.size();
+    int sum = 0;
+    int max_len = 0;
+    int i = 0;
+    for(int j = 0;j<n;j++){
+        sum += v[j];
+        while(sum>target && i < j){
+            sum -= v[i];
+            i++;
+        }
+        if(sum == target){
+          max_len = max(max_len, j - i +1);
+        }
+    }
+    
+    cout<<"Longest subarray which have the sum = "<<target<<" is "<<max_len;
+  
+    return 0;
+}
