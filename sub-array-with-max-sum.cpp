28b9de3b860f81e1416fbaac45e9a0721875903b
Aryan Tiwari <146500011+aryan19022004@users.noreply.github.com>
2026-04-08 18:48:20 +0530

    Implement maximum subarray sum algorithm

--- sub-array-with-max-sum.cpp
+++ sub-array-with-max-sum.cpp
@@ -20,3 +20,38 @@ int main() {
     
     return 0;
 }
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> arr = {-2,1,-3,4,-1,2,1,-5,4};
+
+    int sum = 0, maxSum = INT_MIN;
+    int start = 0, end = 0, tempStart = 0;
+
+    for(int i = 0; i < arr.size(); i++){
+        sum += arr[i];
+
+        if(sum > maxSum){
+            maxSum = sum;
+            start = tempStart;
+            end = i;
+        }
+
+        if(sum < 0){
+            sum = 0;
+            tempStart = i + 1; // new subarray start
+        }
+    }
+
+    cout << "Max Sum: " << maxSum << endl;
+    cout << "Subarray: ";
+
+    for(int i = start; i <= end; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
